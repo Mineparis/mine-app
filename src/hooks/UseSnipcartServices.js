df64d8dcb72bac9200e7dd75772efe56721363a4
gphilips <gphilips@student.42.fr>
2021-11-27 16:26:44 +0100

    fix: hide header when the cart is opened

--- src/hooks/UseSnipcartServices.js
+++ src/hooks/UseSnipcartServices.js
@@ -2,17 +2,17 @@ import { useEffect } from 'react';
 
 const useSnipcartServices = ({ setHideHeader }) => {
 	useEffect(() => {
-		document.addEventListener('snipcart.ready', () => {
-			Snipcart.events.on('theme.routechanged', ({ from, to }) => {
-				if (from === "/" && to !== "/") {
-					setHideHeader(true);
-				}
+		const unsubscribe = Snipcart.events.on('theme.routechanged', ({ from, to }) => {
+			if (from === "/" && to !== "/") {
+				setHideHeader(true);
+			}
 
-				if (from !== "/" && to === "/") {
-					setHideHeader(false);
-				}
-			});
+			if (from !== "/" && to === "/") {
+				setHideHeader(false);
+			}
 		});
+
+		return () => unsubscribe();
 	}, []);
 };
 
