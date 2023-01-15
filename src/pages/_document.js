f8cef3a0e18cd23f28faff5b66cd637fc955ab53
gphilips <gphilips@student.42.fr>
2023-01-15 01:40:16 +0100

    refacto: change the initialization of snipcart

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -26,15 +26,11 @@ class MyDocument extends Document {
 						rel="stylesheet"
 						href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&display=swap"
 					/>
-					<link rel="preconnect" href="https://app.snipcart.com" />
-					<link rel="preconnect" href="https://cdn.snipcart.com" />
-					<link rel="stylesheet" href="https://cdn.snipcart.com/themes/v3.3.0/default/snipcart.css" />
 					<link rel="icon" href="/img/favicon.png" />
 					<link href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" rel="stylesheet" />
 					<meta property="og:type" content="website" />
 					<meta name="google-site-verification" content="HomFVDjGLE7Fgz0LBnFFcDZouzvQmYB4Om_FyvTYh3s" />
 
-					<script async defer src="https://cdn.snipcart.com/themes/v3.3.0/default/snipcart.js" />
 					<script async defer src="https://cdn.ckeditor.com/ckeditor5/31.1.0/classic/ckeditor.js" />
 					<Script src="https://code.jquery.com/jquery-3.4.1.min.js" strategy="lazyOnload" />
 
@@ -67,13 +63,20 @@ class MyDocument extends Document {
 				<body>
 					<Main />
 					<NextScript />
-					<div
-						hidden
-						id="snipcart"
-						data-api-key={process.env.NEXT_PUBLIC_SNIPCART}
-						data-config-modal-style="side"
-						data-currency="eur"
-					/>
+					<script dangerouslySetInnerHTML={{
+						__html: `
+							window.SnipcartSettings = {
+								publicApiKey: "${process.env.NEXT_PUBLIC_SNIPCART}",
+								loadStrategy: "on-user-interaction",
+								modalStyle: "side",
+								currency: "eur",
+								version: "3.4.1"
+							};
+  						(function(){var c,d;(d=(c=window.SnipcartSettings).version)!=null||(c.version="3.0");var s,S;(S=(s=window.SnipcartSettings).timeoutDuration)!=null||(s.timeoutDuration=2750);var l,p;(p=(l=window.SnipcartSettings).domain)!=null||(l.domain="cdn.snipcart.com");var w,u;(u=(w=window.SnipcartSettings).protocol)!=null||(w.protocol="https");var m,g;(g=(m=window.SnipcartSettings).loadCSS)!=null||(m.loadCSS=!0);var y=window.SnipcartSettings.version.includes("v3.0.0-ci")||window.SnipcartSettings.version!="3.0"&&window.SnipcartSettings.version.localeCompare("3.4.0",void 0,{numeric:!0,sensitivity:"base"})===-1,f=["focus","mouseover","touchmove","scroll","keydown"];window.LoadSnipcart=o;document.readyState==="loading"?document.addEventListener("DOMContentLoaded",r):r();function r(){window.SnipcartSettings.loadStrategy?window.SnipcartSettings.loadStrategy==="on-user-interaction"&&(f.forEach(function(t){return document.addEventListener(t,o)}),setTimeout(o,window.SnipcartSettings.timeoutDuration)):o()}var a=!1;function o(){if(a)return;a=!0;let t=document.getElementsByTagName("head")[0],n=document.querySelector("#snipcart"),i=document.querySelector('src[src^="'.concat(window.SnipcartSettings.protocol,"://").concat(window.SnipcartSettings.domain,'"][src$="snipcart.js"]')),e=document.querySelector('link[href^="'.concat(window.SnipcartSettings.protocol,"://").concat(window.SnipcartSettings.domain,'"][href$="snipcart.css"]'));n||(n=document.createElement("div"),n.id="snipcart",n.setAttribute("hidden","true"),document.body.appendChild(n)),h(n),i||(i=document.createElement("script"),i.src="".concat(window.SnipcartSettings.protocol,"://").concat(window.SnipcartSettings.domain,"/themes/v").concat(window.SnipcartSettings.version,"/default/snipcart.js"),i.async=!0,t.appendChild(i)),!e&&window.SnipcartSettings.loadCSS&&(e=document.createElement("link"),e.rel="stylesheet",e.type="text/css",e.href="".concat(window.SnipcartSettings.protocol,"://").concat(window.SnipcartSettings.domain,"/themes/v").concat(window.SnipcartSettings.version,"/default/snipcart.css"),t.prepend(e)),f.forEach(function(v){return document.removeEventListener(v,o)})}function h(t){!y||(t.dataset.apiKey=window.SnipcartSettings.publicApiKey,window.SnipcartSettings.addProductBehavior&&(t.dataset.configAddProductBehavior=window.SnipcartSettings.addProductBehavior),window.SnipcartSettings.modalStyle&&(t.dataset.configModalStyle=window.SnipcartSettings.modalStyle),window.SnipcartSettings.currency&&(t.dataset.currency=window.SnipcartSettings.currency),window.SnipcartSettings.templatesUrl&&(t.dataset.templatesUrl=window.SnipcartSettings.templatesUrl))}})();
+					`
+					}}>
+					</script>
+
 				</body>
 			</Html>
 		);
